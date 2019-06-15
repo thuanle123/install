6c2cf0996bdc9b3bec44ab90c30dd80c456fd207
Thuan Le <38892711+thuanle123@users.noreply.github.com>
2019-06-14 23:31:30 -0700

    Update install.sh
    update docker

--- install.sh
+++ install.sh
@@ -25,8 +25,14 @@ sudo apt-get install git -y
 sudo apt-get upgrade -y
 
 #remove Docker verison and install new version
-sudo apt-get remove docker docker-engine docker.io containerd runc
-sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+#https://linuxize.com/post/how-to-install-and-use-docker-on-ubuntu-18-04/
+sudo apt update
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt updatesudo apt install docker-ce
+sudo systemctl status docker
+
 
 #install java
 sudo apt-get install default-jre -y
